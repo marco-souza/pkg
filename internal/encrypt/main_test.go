bd5d7ab15c2a585fe442a9ec12a76d434e0cf803
Marco Antônio <ma.souza.junior@gmail.com>
2024-07-31 16:57:47 -0300

    fix: update test import

--- internal/encrypt/main_test.go
+++ internal/encrypt/main_test.go
@@ -1,12 +1,15 @@
 package encrypt_test
 
 import (
-	"github.com/marco-souza/pkg/cmd/encrypt"
+	"os"
 	"testing"
+
+	"github.com/marco-souza/pkg/internal/encrypt"
 )
 
 func TestMain(t *testing.T) {
 	// Add your test here
 	encrypt.EncryptFile("main.go", "password")
 	encrypt.DecryptFile("main.go.gpg", "password")
+	os.Remove("main.go.gpg")
 }
