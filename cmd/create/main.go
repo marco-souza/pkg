accd10bc1bebc0f82ba20b608a1500893f033b86
Marco Antônio <ma.souza.junior@gmail.com>
2024-07-31 14:52:24 -0300

    fea: create pkgs

---
+++ cmd/create/main.go
@@ -0,0 +1,78 @@
+/** This package creates a new package to your golang project */
+package create
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+// CreatePackage creates a new package to your golang project
+func CreatePackage(packageName, folder string) {
+	if packageName == "" {
+		fmt.Println("please provide a package name")
+		os.Exit(1)
+	}
+	packageName = strings.ToLower(packageName)
+
+	folder = filepath.Join(folder, packageName)
+	os.MkdirAll(folder, os.ModePerm)
+
+	// Create the main.go file
+	mainFile := filepath.Join(folder, "main.go")
+	f, err := os.Create(mainFile)
+	if err != nil {
+		fmt.Println("error creating main.go file", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("creating package ", packageName, " in ", folder)
+
+	// Write the main.go file
+	f.WriteString(fmt.Sprintf("package %s\n\n", packageName))
+	f.WriteString("import (\n")
+	f.WriteString("\t\"fmt\"\n")
+	f.WriteString(")\n\n")
+
+	f.WriteString("func main() {\n")
+	f.WriteString("\tfmt.Println(\"Hello, World!\")\n")
+	f.WriteString("}\n")
+
+	f.Close()
+
+	// create the main_test.go file
+	testFile := filepath.Join(folder, "main_test.go")
+	f, err = os.Create(testFile)
+	if err != nil {
+		fmt.Println("error creating main_test.go file", err)
+		os.Exit(1)
+	}
+
+	// get module name from go.mod
+	cmd := exec.Command("go", "list", "-m")
+	cmd.Dir = folder
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println("error getting module name", err)
+		os.Exit(1)
+	}
+
+	modulePath := path.Join(strings.TrimSpace(string(out)), folder)
+	fmt.Println("creating tests for ", modulePath)
+
+	// Write the main_test.go file
+	f.WriteString(fmt.Sprintf("package %s_test\n\n", packageName))
+	f.WriteString("import (\n")
+	f.WriteString(fmt.Sprintf("\t\"%s\"\n", modulePath))
+	f.WriteString("\t\"testing\"\n")
+	f.WriteString(")\n\n")
+
+	f.WriteString("func TestMain(t *testing.T) {\n")
+	f.WriteString("\t// Add your test here\n")
+	f.WriteString("}\n")
+
+	f.Close()
+}
--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/marco-souza/pkg/cmd/create"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("command not found")
+		os.Exit(1)
+	}
+
+	cmd := os.Args[1]
+	switch cmd {
+	case "create":
+		if len(os.Args) < 3 {
+			fmt.Println("package not found")
+			os.Exit(1)
+		}
+		packageName := os.Args[2]
+
+		if len(os.Args) < 4 {
+			fmt.Println("folder not found")
+		}
+		folder := os.Args[3]
+
+		create.CreatePackage(packageName, folder)
+	default:
+		fmt.Println("command not found")
+	}
 }
